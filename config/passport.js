25e40263924b187a93192a077f145e1525516063
gkao123 <gkao123@brandeis.edu>
2018-07-17 09:12:43 -0400

    local-signup added

--- config/passport.js
+++ config/passport.js
@@ -3,8 +3,23 @@ var LocalStrategy    = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
 var GoogleStrategy   = require('passport-google-oauth').OAuth2Strategy;
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  
+  passport.deserializeUser((id, cb) => {
+    db.query('SELECT id, username, type FROM users WHERE id = $1', [parseInt(id, 10)], (err, results) => {
+      if(err) {
+        winston.error('Error when selecting user on session deserialize', err)
+        return cb(err)
+      }
+  
+      return cb(null, results.rows[0])
+    })
+  })
+
 passport.use('local-login', new LocalStrategy((username, password, done) => {
-    db.query('SELECT id, username, password, type FROM users WHERE username=$1', [username], (err, result) => {
+    db.query('SELECT username, password FROM users WHERE username=$1', [username], (err, result) => {
       if(err) {
         winston.error('Error when selecting user on login', err)
         return done(null, false, req.flash('loginMessage', 'No user found.'));
@@ -23,56 +38,87 @@ passport.use('local-login', new LocalStrategy((username, password, done) => {
          return done(null, false)
        }
     })
-  }))
-passport.use('local-signup', new LocalStrategy({
-      usernameField : 'email',
-      passwordField : 'password',
-      passReqToCallback : true 
-  },
-  function(req, email, password, done) {
-
-      // asynchronous
-      process.nextTick(function() {
-        db.query('SELECT id, username, password, type FROM users WHERE username=$1', [username], (err, result) => {
-          if(err) {
-            winston.error('Error when selecting user on login', err)
-            return done(null, false, req.flash('loginMessage', 'No user found.'));
-          }
-        }
-          User.findOne({'local.email': email}, function(err, existingUser) {
-              if (err)
-                  return done(err);
-              if (existingUser)
-                  return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-
-              //  If we're logged in, we're connecting a new local account.
-              if(req.user) {
-                  var user            = req.user;
-                  user.local.email    = email;
-                  user.local.password = user.generateHash(password);
-                  user.save(function(err) {
-                      if (err)
-                          throw err;
-                      return done(null, user);
-                  });
-              }
-              //  We're not logged in, so we're creating a brand new user.
-              else {
-                  // create the user
-                  var newUser            = new User();
-
-                  newUser.local.email    = email;
-                  newUser.local.password = newUser.generateHash(password);
-
-                  newUser.save(function(err) {
-                      if (err)
-                          throw err;
-
-                      return done(null, newUser);
-                  });
-              }
-
-          });
-      });
-
   }));
+  passport.use('local-signup', new LocalStrategy((username, password, done) => {
+    hashPW = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    db.query('INSERT INTO USERS VALUES {username, password} FROM users WHERE username=$1 AND password=$2 ', [username, password], (err, result) => {
+      if(err) {
+        winston.error('Existing User', err)
+        return done(null, false, req.flash('existinguser', 'The username is taken!'));
+      } else{
+        return done(null, {username: username})
+      }
+    })))
+
+  passport.use(new FacebookStrategy({
+
+    clientID        : configAuth.facebookAuth.clientID,
+    clientSecret    : configAuth.facebookAuth.clientSecret,
+    callbackURL     : configAuth.facebookAuth.callbackURL,
+    passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+
+},
+function(req, token, refreshToken, profile, done) {
+
+    // asynchronous
+    process.nextTick(function() {
+
+        // check if the user is already logged in
+        if (!req.user) {
+
+            User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+                if (err)
+                    return done(err);
+
+                if (user) {
+
+                    // if there is a user id already but no token (user was linked at one point and then removed)
+                    if (!user.facebook.token) {
+                        user.facebook.token = token;
+                        user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                        user.facebook.email = profile.emails[0].value;
+
+                        user.save(function(err) {
+                            if (err)
+                                throw err;
+                            return done(null, user);
+                        });
+                    }
+
+                    return done(null, user); // user found, return that user
+                } else {
+                    // if there is no user, create them
+                    var newUser            = new User();
+
+                    newUser.facebook.id    = profile.id;
+                    newUser.facebook.token = token;
+                    newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                    newUser.facebook.email = profile.emails[0].value;
+
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, newUser);
+                    });
+                }
+            });
+
+        } else {
+            // user already exists and is logged in, we have to link accounts
+            var user            = req.user; // pull the user out of the session
+
+            user.facebook.id    = profile.id;
+            user.facebook.token = token;
+            user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+            user.facebook.email = profile.emails[0].value;
+
+            user.save(function(err) {
+                if (err)
+                    throw err;
+                return done(null, user);
+            });
+
+        }
+    });
+
+}));
