393d873e36fec29e82434d158f293e698f14be37
Anthony Liu <anthonyliu@brandeis.edu>
2018-07-29 09:30:15 -0700

    renamed Buyere to Awon

--- webapp_js/src/index.js
+++ webapp_js/src/index.js
@@ -4,5 +4,56 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class HelloWorld extends React.Component {
+  render(){
+    return(
+      <div>Hello World!</div>
+    )
+  }
+}
+
+class Awon extends React.Component {
+  render(){
+    return (
+      <div>
+        <h3>awon THE APP COMING SOON!</h3>
+      <HelloUser handleChange = 'anthony'/>
+      </div>
+    )
+  }
+}
+
+class HelloUser extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      username: 'anthonyliu'
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        Hello {this.state.username} <br />
+      Change Name:
+      <input>
+        type = "text"
+        value = {this.state.username}
+        onChange = {this.handleChange}
+      </input>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Awon />, document.getElementById('root'));
+
 registerServiceWorker();
