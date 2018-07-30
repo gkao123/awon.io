612958feaaf39d98beeb0df58b6d6953be9c6ef5
gkao123 <gkao123@brandeis.edu>
2018-07-29 22:59:00 -0400

    mongoose/mlab implmeneted with pooling

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
-const pg = require('pg')
 const config = require('../config')
-const winston = require('winston')
+
+const mongoose = require('mongoose')
+const db_URI = 'mongodb://tbd:tbd@ds149437.mlab.com:49437/statcart'
+console.log('db returned')
 
 const dbConfig = {
 	user: config.db.user,
@@ -12,14 +14,27 @@ const dbConfig = {
 	idleTimeoutMillis: config.db.idleTimeoutMillis,
 }
 
-const pool = new pg.Pool(dbConfig)
-pool.on('error', function (err) {
-	winston.error('idle client error', err.message, err.stack)
-})
 
-module.exports = {
-	pool,
-	query: (text, params, callback) => {
-		return pool.query(text, params, callback)
-	}
+function connect(){
+	const options = {
+		useMongoClient: true,
+		autoIndex: false, // Don't build indexes
+		reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+		reconnectInterval: 500, // Reconnect every 500ms
+		poolSize: 10, // Maintain up to 10 socket connections
+		// If not connected, return errors immediately rather than waiting for reconnect
+		bufferMaxEntries: 0,
+		connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+		socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+		family: 4 // Use IPv4, skip trying IPv6
+	};
+	mongoose.createConnection(db_URI, options, function(error){
+	  console.log('mongodb connected')
+	})
+	db = mongoose.connection
+	return db
+  }
+
+module.exports = function(){
+	connect()
 }
